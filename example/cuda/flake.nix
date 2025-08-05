8ac48ef73a838e71ef1babd5a940233988335789
Plopmenz <plopmenz@gmail.com>
2025-08-05 21:44:09 +0200

    cuda example & cache

---
+++ example/cuda/flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    xnode-manager.url = "github:Openmesh-Network/xnode-manager";
+    xnode-ai-chat.url = "github:OpenxAI-Network/xnode-ai-chat";
+    nixpkgs.follows = "xnode-ai-chat/nixpkgs";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://openxai.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "openxai.cachix.org-1:3evd2khRVc/2NiGwVmypAF4VAklFmOpMuNs1K28bMQE="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
+
+  outputs = inputs: {
+    nixosConfigurations.container = inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        inputs.xnode-manager.nixosModules.container
+        {
+          services.xnode-container.xnode-config = {
+            host-platform = ./xnode-config/host-platform;
+            state-version = ./xnode-config/state-version;
+            hostname = ./xnode-config/hostname;
+          };
+        }
+        inputs.xnode-ai-chat.nixosModules.default
+        (
+          { pkgs, ... }@args:
+          {
+            # START USER CONFIG
+            services.xnode-ai-chat.defaultModel = "deepseek-r1";
+
+            services.ollama.acceleration = "cuda";
+            hardware.graphics = {
+              enable = true;
+              extraPackages = [
+                pkgs.nvidia-vaapi-driver
+              ];
+            };
+            hardware.nvidia.open = true;
+            services.xserver.videoDrivers = [ "nvidia" ];
+            # END USER CONFIG
+
+            services.xnode-ai-chat.enable = true;
+
+            networking.firewall.allowedTCPPorts = [
+              8080
+            ];
+          }
+        )
+      ];
+    };
+  };
+}
